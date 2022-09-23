cc3c868c76c1fec3bdf827959650b548cd244485
Brian96086 <brian96086@gmail.com>
2022-09-22 18:09:15 -0700

    add PA1 startercode

---
+++ PA1/ProgrammingChallenges.java
@@ -0,0 +1,65 @@
+/**
+ * Name: TODO
+ * PID: TODO
+ */
+
+/**
+ * TODO
+ * @author TODO
+ * @since TODO
+ */
+public class ProgrammingChallenges {
+    
+    /////// Challenge 1 ///////
+    public static boolean store(String item, float inWallet, float needed) {
+        //TODO
+        return true;
+    }
+    /////// Challenge 2 ///////
+    public static boolean sameDigitFirstAndLast (int num1, int  num2) {
+        //TODO
+        return true;
+    }
+    /////// Challenge 3 ///////
+    public static boolean decreasingOrder (int[] elems) {
+        //TODO
+        return true;
+    }
+
+    /////// Challenge 4 ///////
+    public static float averageGrade(int[][] grades, int assignmentIndex) {
+        //TODO
+        return (float)0.0;
+    }
+
+    /////// Challenge 5 ///////
+    public static int secondMax(int[] a) {
+        //TODO
+        return 0;
+    }
+
+    /////// Challenge 6 ///////
+    public static String noDots(String str) {
+        //TODO
+        return null;
+    }
+
+    /////// Challenge 7 ///////
+    public static int[] twoElements (int[] elems) {
+        //TODO
+        return null;
+    }
+
+    /////// Challenge 8 ///////
+    public static String checkPasswordStrength(String input) {
+        //TODO
+        return null;
+    }
+
+    /////// Challenge 9 ///////
+    public static int[] multiplyByGroup (int[] arr, int groupSize) {
+        //TODO
+        return null;
+    }
+    //If you want to implement part3, you have to create a new java file(see writeup!)
+}
